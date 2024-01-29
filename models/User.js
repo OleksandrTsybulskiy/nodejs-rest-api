f705fbb5494d67a0cda42317fe87b61d1bcdd737
OleksandrTsybulskiy <126475884+OleksandrTsybulskiy@users.noreply.github.com>
2024-01-30 00:25:24 +0200

    hw4

---
+++ models/User.js
@@ -0,0 +1,41 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import { handleSaveError, preUpdate } from "./hooks.js";
+
+const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+const userSchema = new Schema(
+	{
+		password: {
+			type: String,
+			required: [true, "Set password for user"],
+		},
+		email: {
+			type: String,
+			required: [true, "Email is required"],
+			unique: true,
+		},
+		token: String,
+	},
+	{ versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", preUpdate);
+
+userSchema.post("findOneAndUpdate", handleSaveError)
+
+export const userSignupScheme = Joi.object({
+    password: Joi.string().required(),
+    email: Joi.string().required(),
+})
+
+export const userSigninScheme = Joi.object({
+    password: Joi.string().required(),
+    email: Joi.string().pattern(emailPattern).required()
+})
+
+const User = model('user', userSchema)
+
+export default User
---
+++ routes/api/auth-router.js
@@ -0,0 +1,18 @@
+import express from "express"
+import authController from "../../controllers/auth-controller.js"
+import { isEmptyBody}  from "../middlewares/index.js";
+import validateBody from "../decorators/validateBody.js";
+import { userSigninScheme, userSignupScheme } from "../models/User.js";
+import authenticate from "../middlewares/authenticate.js";
+
+const authRouter = express.Router()
+
+authRouter.post('/register', isEmptyBody, validateBody(userSignupScheme), authController.singup)
+
+authRouter.post('/login', isEmptyBody, validateBody(userSigninScheme), authController.singin)
+
+authRouter.post('/logout', authenticate, authController.signout)
+
+authRouter.get('/current', authenticate,  authController.getCurrent)
+
+export default authRouter
