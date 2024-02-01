a87be8c2312a3721c3c55acefa6b2b48918409df
OleksandrTsybulskiy <126475884+OleksandrTsybulskiy@users.noreply.github.com>
2024-02-01 19:42:31 +0200

    hw5

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -17,6 +17,6 @@ authRouter.post('/logout', authenticate, authController.signout)
 
 authRouter.get('/current', authenticate, authController.getCurrent)
 
-authRouter.patch('/avatars', upload.single("avatarURL"), isEmptyBody, resizeAvatar, authenticate, authController.updateAvatar)
+authRouter.patch('/avatars', authenticate, upload.single("avatarURL"), isEmptyBody, resizeAvatar, authController.updateAvatar)
 
 export default authRouter
